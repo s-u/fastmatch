29067b97fe3dfeb43a363ca1a8fae5ab26f11479
Simon Urbanek <simon.urbanek@r-project.org>
2017-01-28 09:09:12 -0500

    save an extra fn call in %fin%

--- R/fastmatch.R
+++ R/fastmatch.R
@@ -5,4 +5,4 @@ fmatch.hash <- function(x, table, nomatch = NA_integer_, incomparables = NULL)
   .Call(C_fmatch, x, table, nomatch, incomparables, TRUE)
 
 `%fin%` <- function (x, table)
-  fmatch(x, table, nomatch = 0L) > 0L
+  .Call(C_fmatch, x, table, 0L, NULL, FALSE) > 0L
